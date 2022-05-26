017399e934ececa60766f1a5ece964c243d8ada8
Farhan-Abid <shahabid628@gmail.com>
2022-05-26 22:40:06 +0600

    git added server

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// jwt token verify
 const verifyJwt = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
