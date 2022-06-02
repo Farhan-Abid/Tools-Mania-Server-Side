106919cb0813f6680a4bd57a90073257ec01e49f
Farhan-Abid <shahabid628@gmail.com>
2022-06-02 21:58:42 +0600

    msg

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin : "https://manufacturer-website-cli-78254.web.app/" }))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivt5b.mongodb.net/?retryWrites=true&w=majority`;
@@ -75,17 +75,17 @@ async function run(){
             res.send({ admin: isAdmin });
           });
       
-          app.put("/user/admin/:email", verifyJwt, verifyAdmin, async (req, res) => {
-            const email = req.params.email;
-            const filter = { email: email };
+          // app.put("/user/admin/:email", verifyJwt, verifyAdmin, async (req, res) => {
+          //   const email = req.params.email;
+          //   const filter = { email: email };
       
-            const updateDoc = {
-              $set: { role: "admin" },
-            };
+          //   const updateDoc = {
+          //     $set: { role: "admin" },
+          //   };
       
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
-          });
+          //   const result = await userCollection.updateOne(filter, updateDoc);
+          //   res.send(result);
+          // });
       
         //   put method usage 
           app.put("/user/:email", async (req, res) => {
